5eff04f25feb9df15b904cd895ff3587afe349ac
Lucas <lucas_fasael@hotmail.com>
2023-03-03 19:15:23 -0300

    target_att

--- target2.py
+++ target2.py
@@ -1,21 +1,25 @@
 lista = [0, 1]
 z = 0
-cont = 0
+isFibonacci = False
 y = int(input("Qual o número a ser testado ? "))
 
-if (y == 1):
+def whenFibonacci():
     print("O número {} digitado pertence a sequência de Fibonacci !".format(y))
-    cont = cont + 1
+    global isFibonacci
+    isFibonacci = True
+
+# Foi feito um if inicial para evitar a duplicação da frase quando detectasse que o número "1" pertence duas vezes ao grupo dos Fibonacci !
+
+
+if (y == 1):
+    whenFibonacci()
 else:
     while (y >= lista[z]):
         fib = lista[z+1] + lista[z]
         lista.append(fib)
         if (y == lista[z]):
-            print("O número {} digitado pertence a sequência de Fibonacci !".format(y))
-            cont = cont + 1
+            whenFibonacci()
         z = z + 1
 
-if (cont == 0):
+if (isFibonacci == False):
     print("O número {} digitado NÃO pertence a sequência de Fibonacci !".format(y))
-
-# Foi feito um if inicial para evitar a duplicação da frase quando detectasse que o número "1" pertence duas vezes ao grupo dos Fibonacci !
--- target5.py
+++ target5.py
@@ -1,12 +1,25 @@
-lista = []
-y = 0
-x = input("Digite um texto a ser invertido : ")
-tamanho = len(x)
+# Primeiramente tive essa ideia:
 
-while (len(x) > y):
-    lista.append(x[tamanho-1])
-    tamanho = tamanho - 1
-    y = y + 1
+# lista = []
+# y = 0
+# x = input("Digite um texto a ser invertido : ")
+# tamanho = len(x)
 
-s = ''.join(lista)
-print(s)
+# while (len(x) > y):
+#     lista.append(x[tamanho-1])
+#     tamanho = tamanho - 1
+#     y = y + 1
+
+# s = ''.join(lista)
+# print(s)
+
+# Mas depois refatorei e fiz um código mais enxuto:
+
+listaCaracteres = input("Digite um texto a ser invertido : ")
+
+
+resultado = ""
+for caracter in listaCaracteres:
+    resultado = caracter + resultado
+
+print(resultado)
